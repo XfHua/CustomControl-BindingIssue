5b5c45f19429b22d5a5db08951b870916e00a6d2
XfHua <jasonHuaxf@163.com>
2020-01-03 09:48:38 +0800

    Add files via upload

---
+++ App110/App110/App110/MainPage.xaml.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace App110
+{
+    // Learn more about making custom code visible in the Xamarin.Forms previewer
+    // by visiting https://aka.ms/xamarinforms-previewer
+    [DesignTimeVisible(false)]
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+
+            ExampleViewModel vm = new ExampleViewModel();
+            vm.Message = "test";
+            BindingContext = vm;
+        }
+    }
+
+    public class ExampleViewModel : INotifyPropertyChanged
+    {
+        private string _message;
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public string Message
+        {
+            set
+            {
+                if (_message != value)
+                {
+                    _message = value;
+                    if (PropertyChanged != null)
+                    {
+                        PropertyChanged(this, new PropertyChangedEventArgs("Message"));
+                    }
+                }
+            }
+            get
+            {
+                return _message;
+            }
+        }
+
+        public ExampleViewModel()
+        {
+            Message = "Message from viewModel!";
+        }
+    }
+}
---
+++ App110/App110/App110/MessageBar.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace App110
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class MessageBar : ContentView
+    {
+        public static readonly BindableProperty MessageTextProperty = BindableProperty.Create(
+            propertyName: nameof(MessageText),
+            returnType: typeof(string),
+            declaringType: typeof(MessageBar),
+            defaultValue: default(string));
+
+        public string MessageText
+        {
+            get { return (string)GetValue(MessageTextProperty); }
+            set { SetValue(MessageTextProperty, value); }
+        }
+
+        public MessageBar()
+        {
+            InitializeComponent();
+        }
+    }
+}
